1bd561ff1e436290c7ce4b10943842d6c22d6651
prashantk <prashantk@aweber.com>
2024-02-07 23:48:24 +0530

    Parse JSON Files

--- germany/app.py
+++ germany/app.py
@@ -1,3 +1,5 @@
+import os
+import json
 import secrets
 
 import flask
@@ -9,6 +11,38 @@ secret_key = secrets.token_urlsafe(32)
 app.secret_key = secret_key
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = secret_key
+
+
+@app.route('/map')
+def map_json():
+    path = f'{os.getcwd()}/germany/data'
+
+    with open(f'{path}/relation/rel.json', 'r') as f:
+        relations = json.loads(f.read()).get('relation')
+
+    findings = []
+    for cfile in os.listdir(f'{path}/json'):
+        print(f'Parsing {cfile}')
+        with open(f'{path}/json/{cfile}', 'r') as f:
+            data = json.loads(f.read())
+        parse_json(data, relations, findings)
+
+    print(json.dumps(findings, indent=4))
+    return "Something i am doing...."
+
+
+def parse_json(data, relations, findings):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key in relations:
+                findings.append(data)
+            elif value and isinstance(value, (list, dict)):
+                parse_json(value, relations, findings)
+    elif isinstance(data, list):
+        for li in data:
+            if li:
+                parse_json(li, relations, findings)
+    return None
 
 
 @app.route('/status')
